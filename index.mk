19ca234697d67fe42b11b6595397eef10dc64d52
Amandine <145350740+AmandineDC@users.noreply.github.com>
2023-10-09 11:23:51 +0200

    index.mk

---
+++ index.mk
@@ -0,0 +1,22 @@
+# Amandine Ducrosc Cervos
+
+## À propos de moi
+Étudiante passionnée en information-communication avec une solide expérience en gestion de projets, ainsi qu'en création de contenus.
+Ayant effectué plusieurs stages dans le domaine : agence de communication, radio. 
+Je suis à la recherche de nouvelles expériences pour approfondir mes compétences. 
+
+## Formation
+- **2021-maintenant:**BUT Information Numérique dans les organisations.
+- **2018-2021:** Diplôme du Baccalauréat, section européenne, mention bien.
+
+## Expériences professionnelles
+- **2022:** Stage chez Exorde, agence de communication bordelaise. 
+
+## Compétences
+- Communication digitale
+- Rédaction web
+- ...
+
+## Contact
+- Email: exemple@email.com
+- LinkedIn: [Mon LinkedIn](https://www.linkedin.com/in/monprofil/)
